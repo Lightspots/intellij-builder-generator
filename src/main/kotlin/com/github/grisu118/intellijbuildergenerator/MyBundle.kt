99481acefd8505b116edc7ccb3017d18fcd91010
GitHub Action <action@github.com>
2020-10-07 06:55:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/grisu118/intellijbuildergenerator/MyBundle.kt
+++ src/main/kotlin/com/github/grisu118/intellijbuildergenerator/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.grisu118.intellijbuildergenerator
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/grisu118/intellijbuildergenerator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.grisu118.intellijbuildergenerator.services
+
+import com.github.grisu118.intellijbuildergenerator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
